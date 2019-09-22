80d6a8632c9018dc0454b6b5a8e02aa1da090c5b
DragoiVladIoan <45821639+DragoiVladIoan@users.noreply.github.com>
2019-09-22 19:46:49 +0300

    Implemented Infinite Scrolling Scenery

---
+++ TestProject2D/Assets/BackgroundScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+
+    public float xVelocity, yVelocity;
+
+    void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
