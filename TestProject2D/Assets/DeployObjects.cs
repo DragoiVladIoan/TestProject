82160580592f74bf055acafca559613aaefaddaf
DragoiVladIoan <45821639+DragoiVladIoan@users.noreply.github.com>
2019-09-22 22:03:42 +0300

    Implemented Interactive Objects Spawn

---
+++ TestProject2D/Assets/DeployObjects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployObjects : MonoBehaviour
+{
+    public GameObject[] interactiveObjects;
+    public float respawTime = 1.0f;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(objectWave());
+    }
+
+    private void spawnObject()
+    {
+        int objectSelected = Random.Range(0, interactiveObjects.Length);
+        GameObject obj = Instantiate(interactiveObjects[objectSelected]) as GameObject;
+        obj.transform.position = new Vector3(screenBounds.x + 3, -3, -2);
+    }
+    
+    IEnumerator objectWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawTime);
+            spawnObject();
+        }
+        
+    }
+}
---
+++ TestProject2D/Assets/EnvironmentObjectMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentObjectMovement : MonoBehaviour
+{
+    public float playerSpeed = 5;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x -= playerSpeed * Time.deltaTime;
+        transform.position = pos;
+        if (Math.Abs(transform.position.x) > screenBounds.x * 2)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
